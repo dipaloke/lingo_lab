08fd6120cb0031eea22b5c68c54eac149bc77c15
dipaloke <dipalokebiswas96@gmail.com>
2024-04-26 13:47:16 +0600

    Modified the landing page a bit, added footer with falgs downloaded from (flagpack.xyz).

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -23,40 +23,42 @@ export default function Home() {
         <h1 className="text-xl lg:text-3xl font-bold text-neutral-600 max-w-[480px] text-center">
           Learn, practice and master new language with Lingo-Lab.
         </h1>
-        {/* spacial cases for if we are logged in or not */}
-        <ClerkLoading>
-          <Loader className="h-5 w-5 text-muted-foreground animate-spin" />
-        </ClerkLoading>
-        <ClerkLoaded>
-          {/* when user is signed out */}
-          <SignedOut>
-            <SignUpButton
-              mode="modal"
-              signInFallbackRedirectUrl="/learn"
-              signInForceRedirectUrl="/learn"
-            >
-              <Button className="w-full" variant="secondary" size="lg">
-                Get Started
-              </Button>
-            </SignUpButton>
+        <div className=" flex flex-col items-center gap-y-3 max-w-[330px] w-full">
+          {/* spacial cases for if we are logged in or not */}
+          <ClerkLoading>
+            <Loader className="h-5 w-5 text-muted-foreground animate-spin" />
+          </ClerkLoading>
+          <ClerkLoaded>
+            {/* when user is signed out */}
+            <SignedOut>
+              <SignUpButton
+                mode="modal"
+                signInFallbackRedirectUrl="/learn"
+                signInForceRedirectUrl="/learn"
+              >
+                <Button className="w-full" variant="secondary" size="lg">
+                  Get Started
+                </Button>
+              </SignUpButton>
 
-            <SignInButton
-              mode="modal"
-              signUpFallbackRedirectUrl="/learn"
-              signUpForceRedirectUrl="/learn"
-            >
-              <Button className="w-full" variant="primaryOutline" size="lg">
-                I already have an account.
+              <SignInButton
+                mode="modal"
+                signUpFallbackRedirectUrl="/learn"
+                fallbackRedirectUrl="/learn"
+              >
+                <Button className="w-full" variant="primaryOutline" size="lg">
+                  I already have an account
+                </Button>
+              </SignInButton>
+            </SignedOut>
+            {/* when user is signed-in */}
+            <SignedIn>
+              <Button size="lg" variant="secondary" asChild className="w-full">
+                <Link href="/learn">Continue learning</Link>
               </Button>
-            </SignInButton>
-          </SignedOut>
-          {/* when user is signed-in */}
-          <SignedIn>
-            <Button size="lg" variant="secondary" asChild>
-              <Link href="/learn">Continue learning</Link>
-            </Button>
-          </SignedIn>
-        </ClerkLoaded>
+            </SignedIn>
+          </ClerkLoaded>
+        </div>
       </div>
     </div>
   );
