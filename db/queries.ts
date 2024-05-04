e2e9edc79f515ebd7dde34ff5285d20123c05b47
dipaloke <dipalokebiswas96@gmail.com>
2024-05-04 21:22:33 +0600

    Course Progress: Defined queries for getCourseProgress and getLessonPercentage, then get access to the courseProgress & lessonData from the learn page. NOTE: ENcountered ERROR: The uncompleated courses are showing as compleated. Will FIX it next.

--- db/queries.ts
+++ db/queries.ts
@@ -1,8 +1,15 @@
 import { cache } from "react";
 import db from "./drizzle";
 import { auth } from "@clerk/nextjs/server";
-import { eq } from "drizzle-orm";
-import { challengeProgress, courses, units, userProgress } from "./schema";
+import { asc, eq } from "drizzle-orm";
+import {
+  challengeProgress,
+  challenges,
+  courses,
+  lessons,
+  units,
+  userProgress,
+} from "./schema";
 
 //need to store the courses as cache so we don't have to pass the data as props
 export const getCourses = cache(async () => {
@@ -81,4 +88,113 @@ export const getCoursesById = cache(async (courseId: number) => {
     //TODO: Populate units & lessons
   });
   return data;
+});
+
+export const getCourseProgress = cache(async () => {
+  const { userId } = await auth();
+  const userPRogress = await getUserProgress();
+
+  if (!userId || !userPRogress?.activeCourseId) {
+    return null;
+  }
+
+  const unitsInActiveCourse = await db.query.units.findMany({
+    orderBy: (units, { asc }) => [asc(lessons.order)],
+    where: eq(units.courseId, userPRogress.activeCourseId),
+    with: {
+      lessons: {
+        orderBy: (lessons, { asc }) => [asc(lessons.order)],
+        with: {
+          unit: true,
+          challenges: {
+            with: {
+              challengeProgress: {
+                where: eq(challengeProgress.userId, userId),
+              },
+            },
+          },
+        },
+      },
+    },
+  });
+  //First uncompleted lesson that will be shown in frontend so users can start from there.
+  const firstUncompletedLesson = unitsInActiveCourse
+    .flatMap((units) => units.lessons)
+    .find((lesson) => {
+      //TODO: If something breaks check the last if clause
+      return lesson.challenges.some((challenge) => {
+        return (
+          !challenge.challengeProgress ||
+          challenge.challengeProgress.length === 0 ||
+          challenge.challengeProgress.some(
+            (progress) => progress.completed === false
+          )
+        );
+      });
+    });
+  return {
+    activeLesson: firstUncompletedLesson,
+    activeLessonId: firstUncompletedLesson?.id,
+  };
+});
+
+//If user provide an id for lesson(user clicked on a specific lesson) then we are going to load it, otherwise we load the first uncompleted lesson from getCourseProgress.
+export const getLesson = cache(async (id?: number) => {
+  const { userId } = await auth();
+
+  if (!userId) return null;
+
+  const courseProgress = await getCourseProgress();
+  const lessonId = id || courseProgress?.activeLessonId;
+
+  if (!lessonId) return null;
+
+  const data = await db.query.lessons.findFirst({
+    where: eq(lessons.id, lessonId),
+    with: {
+      challenges: {
+        orderBy: (challenges, { asc }) => [asc(challenges.order)],
+        with: {
+          challengeOptions: true,
+          challengeProgress: {
+            where: eq(challengeProgress.userId, userId),
+          },
+        },
+      },
+    },
+  });
+
+  if (!data || !data.challenges) return null;
+
+  //normalizing for frontEnd
+
+  const normalizedChallenges = data.challenges.map((challenge) => {
+    //TODO: If something breaks check the last if clause
+    const completed =
+      challenge.challengeProgress &&
+      challenge.challengeProgress.length > 0 &&
+      challenge.challengeProgress.every((progress) => progress.completed);
+
+    return { ...challenge, completed };
+  });
+
+  return { ...data, challenges: normalizedChallenges };
+});
+
+export const getLessonPercentage = cache(async () => {
+  const courseProgress = await getCourseProgress();
+
+  if (!courseProgress?.activeLessonId) return 0;
+
+  const lesson = await getLesson(courseProgress.activeLessonId);
+
+  if (!lesson) return 0;
+
+  const CompletedChallenges = lesson.challenges.filter(
+    (challenge) => challenge.completed
+  );
+  const percentage = Math.round(
+    (CompletedChallenges.length / lesson.challenges.length) * 100
+  );
+  return percentage;
 });
